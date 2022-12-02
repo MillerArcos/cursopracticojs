365fca1a0f2dba98d8a1999c7a43739ff183e1f3
miller <milleragcb@hotmail.com>
2022-12-02 07:28:31 -0500

    cree archivo html y js para leer eventos del usuario

---
+++ DOM/script.js
@@ -0,0 +1,4 @@
+const h1 = document.querySelector("h1");
+const imput1 = document.querySelector("#calculo1");
+const imput2 = document.querySelector("#calculo2");
+const imput1 = document.querySelector("#btnCalcular");
