d51f524dcb099609eabd51f8b5d3a7b2e3cd444f
miller <milleragcb@hotmail.com>
2022-12-07 18:00:06 -0500

    termine los eventos para enviar informacion al HTML desde js cuando el ususario rrealice una accion en especifico

--- DOM/script.js
+++ DOM/script.js
@@ -1,4 +1,11 @@
 const h1 = document.querySelector("h1");
-const imput1 = document.querySelector("#calculo1");
-const imput2 = document.querySelector("#calculo2");
-const imput1 = document.querySelector("#btnCalcular");
+const imput1 = document.querySelector('#calculo1');
+const imput2 = document.querySelector('#calculo2');
+const button = document.querySelector('#btnCalcular');
+const result = document.querySelector('#result');
+
+
+function btOnClick (){
+    const sumaImputs= parseInt(imput1.value) + parseInt(imput2.value);
+    result.innerHTML = "Resultado: " + sumaImputs;
+}
