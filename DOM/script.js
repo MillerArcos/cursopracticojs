a69e6ccaf1273528853ee5fa5865123d76573d1f
miller <milleragcb@hotmail.com>
2022-12-07 18:27:55 -0500

    aprendimos a agregar eventos desde js

--- DOM/script.js
+++ DOM/script.js
@@ -3,9 +3,11 @@ const imput1 = document.querySelector('#calculo1');
 const imput2 = document.querySelector('#calculo2');
 const button = document.querySelector('#btnCalcular');
 const result = document.querySelector('#result');
+const form = document.querySelector('#form');
+form.addEventListener('submit', sumarImputs );
 
-
-function btOnClick (){
+function sumarImputs (event){
+    event.preventDefault();
     const sumaImputs= parseInt(imput1.value) + parseInt(imput2.value);
     result.innerHTML = "Resultado: " + sumaImputs;
 }
